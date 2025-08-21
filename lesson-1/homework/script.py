c55962f0020790ccf81cc4cb3c08917258fa5314
Dizakir <125751963+Dizakir@users.noreply.github.com>
2025-08-21 18:19:17 +0500

    Update script.py

--- lesson-1/homework/script.py
+++ lesson-1/homework/script.py
@@ -9,8 +9,8 @@ print(square_area)
 
 # Task 2. Given diameter of circle. Find its length.
 circle_diameter = int(input('Diameter of the circle:'))
-circle_lenght = 2 * math.pi * circle_diameter / 2
-print(circle_lenght)
+circle_length = 2 * math.pi * circle_diameter / 2
+print(circle_length)
 
 # Task 3. Given two numbers a and b. Find their mean.
 a = int(input('Enter a:'))
@@ -19,11 +19,11 @@ mean_a_b = (a + b)/2
 print(mean_a_b)
 
 # Task 4. Given two numbers a and b. Find their sum, product and square of each number.
-mean_a_b = a + b
+mean2 = a + b
 product_a_b = a * b
 square_a = a**2
 square_b = b**2
-print(mean_a_b)
-print(product_a_b)
+print(mean2)
+print('square: ', product_a_b)
 print(square_a)
 print(square_b)
